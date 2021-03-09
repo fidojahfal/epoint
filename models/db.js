5f68816ac6ffe1a862bdfce45527597a2ac8699c
fidojahfal <42795148+fidojahfal@users.noreply.github.com>
2021-03-10 00:31:54 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: proccess.env.PORT,
+    port: process.env.PORT,
     dateStrings: true
 });
 connection.connect((err) => {
