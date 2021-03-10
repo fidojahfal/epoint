b3d008246eab57688d921b61fd2705cb7f5ca355
fidojahfal <42795148+fidojahfal@users.noreply.github.com>
2021-03-10 10:56:29 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT,
-    dateStrings: true
-});
-connection.connect((err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Connected to the database');
-    }
-});
+// const connection = mysql.createConnection({
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DATABASE,
+//     port: process.env.PORT,
+//     dateStrings: true
+// });
+// connection.connect((err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log('Connected to the database');
+//     }
+// });
 
-module.exports = connection;
+// module.exports = connection;
