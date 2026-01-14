36651242d3753131b0c865e78138ad4250370426
fidojahfal <fidojahfalprayoga@gmail.com>
2026-01-14 23:19:35 +0700

    fix: port always showing 3000

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const expressLayout = require('express-ejs-layouts')
-const path = require('path');
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const expressLayout = require("express-ejs-layouts");
+const path = require("path");
 const app = express();
 var PORT = process.env.PORT || 5000;
 
 //Body Parser
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 //Cookie parser
 app.use(cookieParser());
 
 //Set ejs
 app.use(expressLayout);
-app.use(express.static(path.join(__dirname + '/views/public')));
-app.use(express.static(path.join('../picturedemo/')))
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname + "/views/public")));
+app.use(express.static(path.join("../picturedemo/")));
+app.set("view engine", "ejs");
 
 //Define Routes
-app.use('/', require('./Routes/dashboard'));
-app.use('/auth', require('./Routes/auth'));
+app.use("/", require("./Routes/dashboard"));
+app.use("/auth", require("./Routes/auth"));
 
 app.use((req, res, next) => {
-    const title = "404 not found"
-    res.status(404)
-    res.render('404', { title: title })
-})
+  const title = "404 not found";
+  res.status(404);
+  res.render("404", { title: title });
+});
 
-app.listen(PORT,
-    console.log('Start server at port 3000'));
+app.listen(PORT, console.log(`Start server at port ${PORT}`));
